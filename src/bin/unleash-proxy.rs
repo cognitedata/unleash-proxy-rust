48ce717ec803e1428628b4930f5a378c7846512a
Robert Collins <robert.collins@cognite.com>
2022-03-03 18:25:21 +0100

    Permit customisation of the Proxy
    This facilitates site-local changes without forking.

---
+++ src/bin/unleash-proxy.rs
@@ -0,0 +1,16 @@
+//! Copyright 2020-2022 Cognite AS
+//!
+//! A default configured proxy that should work for many common deployments.
+//!
+//! To configure a custom proxy, including custom feature flags, tracing etc, see `unleash_proxy_rust::main`.
+#![warn(clippy::all)]
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // Deployment specific:
+    // TODO: Add tracing
+    // TODO: Add prometheus
+    // TODO: Add healthz route control
+    env_logger::init();
+    Ok(unleash_proxy_rust::main().await?)
+}
